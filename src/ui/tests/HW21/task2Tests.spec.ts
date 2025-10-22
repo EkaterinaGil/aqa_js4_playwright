15b39e6e1224f600c62360bda432e9dc3e2e367c
Ekaterina Gil <Ekaterina.Gil@gmail.com>
2025-10-23 00:01:05 +0300

    task1 and task2

---
+++ src/ui/tests/HW21/task2Tests.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, Page } from "@playwright/test";
+import testData, { TableRow } from "./testDataForTask2.spec";
+
+
+const url = "https://anatoly-karpovich.github.io/test-automation-sandbox";
+
+async function getTableRow(page: Page, email: string): Promise<TableRow | undefined> {
+    await page.goto(url);
+    await page.locator('[href="/test-automation-sandbox/sortable-table"]').click();
+
+    const table = page.locator(".MuiTable-root");
+    const rows = await table.locator("tbody tr").all();
+    const columnHeaders = await table.locator("thead span").allInnerTexts();
+
+    for (const row of rows) {
+        const rowData: TableRow = {};
+        const cells = await row.locator("td").all();
+        for (let i = 0; i < cells.length - 1; i++) {
+            const headerValue = columnHeaders[i];
+            const cellValue = await cells[i]!.innerText();
+            if (headerValue) { rowData[headerValue] = cellValue }
+        }
+        if (rowData.Email === email) {
+            return rowData;
+        }
+    }
+    return undefined;
+};
+
+test.describe("GetTableRow tests", () => {
+    for (const obj of testData) {
+        test(`${obj.Email}`, async ({ page }) => {
+            const getRow = await getTableRow(page, obj.Email!);
+            expect(getRow).toStrictEqual(obj);
+        });
+    };
+});
